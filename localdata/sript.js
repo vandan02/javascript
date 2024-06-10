d9cd40578c55250531261480259771d8706db1e4
vandan <vandanpatel493@gmail.com>
2024-06-10 20:43:02 +0530

    exam5

--- localdata/sript.js
+++ localdata/sript.js
@@ -1,9 +1,9 @@
 let form=document.querySelector("form")
 form.addEventListener("submit",(e) => {
     let name=e.target.name.value;
-    let emial=e.target.email.value;
+    let email=e.target.email.value;
     let number=e.target.number.value;
-    console.log(name,emial,number);
+    console.log(name,email,number);
     e.preventDefault();
 alert("hello")
 });
