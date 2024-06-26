ac00e855276fadee07a725c9cb80dd9c1930ea50
vandan <vandanpatel493@gmail.com>
2024-06-26 20:52:58 +0530

    add

--- tourist-place/js/wish.js
+++ tourist-place/js/wish.js
@@ -10,30 +10,29 @@ const uimaker=()=>{
  data.map((ele,i)=>{
 let img=document.createElement("img");
 img.src=ele.img1;
+let img2=document.createElement("img");
+img.src=ele.img2;
+let img3=document.createElement("img");
+img.src=ele.img3;
 let h2=document.createElement("h2")
+h2.innerHTML=ele.title;
+let div1=document.createElement("div")
+h2.innerHTML=ele.discription;
+let div=document.createElement("div")
 h2.innerHTML=ele.title;
 let p=document.createElement("p");
 p.innerHTML=ele.price
 
 
-let btn=document.createElement("button");
-btn.innerHTML=`like:${ele.like}`;
-btn.addEventListener("click",()=>like(i))
 
 
-let btn2=document.createElement("button");
-btn2.innerHTML="wishlist"
-btn2.addEventListener("click",()=>wish(i))
 
-let div=document.createElement("div");
-div.append(btn,btn2)
+
+
 let div2=document.createElement("div");
-div2.append(img,h2,p,btn,btn2,div)
+div2.append(img,img2,img3,h2,div,p)
 
-div2.addEventListener("click",(i)=>{
 
-window.location.href="/tourist-place/pages/wishlist.html"
-})
 document.getElementById("show").append(div2);
 
  })
